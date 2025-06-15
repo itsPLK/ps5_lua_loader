c70aba3fb3291e65a6b17c83c62a5dc2aa0c7876
n0llptr <viljosel@proton.me>
2025-06-15 22:29:36 +0300

    Add is patches applied variable to ps4 post exploit

--- savedata/lapse.lua
+++ savedata/lapse.lua
@@ -1764,6 +1764,8 @@ function post_exploitation_ps4()
         kernel.write_dword(sysent_661_addr + 0x2c, sy_thrcnt)
         
         syscall.close(write_handle)
+        
+        kernel.is_ps4_kpatches_applied = true
     end
     
     local function should_apply_kernel_patches()
@@ -1781,6 +1783,7 @@ function post_exploitation_ps4()
     end
     
     -- Run post-exploit logic
+    kernel.is_ps4_kpatches_applied = false
     local proc = kernel.addr.curproc
     calculate_kbase(evf_ptr)
     printf("Kernel Base Candidate: %s", hex(kernel.addr.data_base))
